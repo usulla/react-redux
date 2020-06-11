d26a08ebff9ee2c1eec62352e76845f5e48956f1
Сухарева Мария <maria_suxareva@mail.ru>
2020-06-11 22:07:04 +0300

    added async fetch posts

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { compose, createStore, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import { rootReducer } from './redux/rootReducer'
+import thunk from 'redux-thunk'
+import { forbiddenWordsMiddleware } from './redux/middleware';
 
-ReactDOM.render(
-  <React.StrictMode>
+const store = createStore(rootReducer, compose(
+  applyMiddleware(
+    thunk, forbiddenWordsMiddleware
+  ),
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
+const app = (
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>
+)
+render(
+  app,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
